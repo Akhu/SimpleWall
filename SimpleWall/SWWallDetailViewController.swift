db3a0566819d1725ccfa48e5b79d2229545ea736
Anthony Da Cruz <anthony.dacruz@aboutgoods.net>
2016-11-04 21:45:17 +0100

    Done : * Loader --------------------------------------- Todo : *

--- SimpleWall/SWWallDetailViewController.swift
+++ SimpleWall/SWWallDetailViewController.swift
@@ -22,33 +22,19 @@ class SWWallDetailViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var wallInfo: SWWallInfosView!
     
-    var imageToSave:UIImageView?
+    @IBOutlet weak var loaderView: UIView!
     
     @IBOutlet weak var progressBarDownload: UIProgressView!
     
+    @IBOutlet weak var progressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         if let imageURL = self.wall?.imageWall?.regularSizeUrl {
             Nuke.loadImage(with: imageURL, into: self.wallImage)
         }
-        self.imageToSave = UIImageView()
         
-        print(self.wall?.imageWall?.smallSizeUrl)
-        
-//        Alamofire.download(URL(string: "https://images.unsplash.com/photo-1477290459437-16a3a5308623?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&s=50a860e069beb7ac070599d4e2f33bf2")!)
-//            .downloadProgress(closure: { (progress) in
-//                print("Download Progress: \(progress.fractionCompleted*100.rounded())")
-//            })
-//            .response { response in
-//                print(response.result)
-//                
-//                if let data = response.result.value {
-//                    self.imageToSave?.image = UIImage(data: data)
-//                    UIImageWriteToSavedPhotosAlbum(self.wallImage.image!, self, Selector(("imageSavingCompletionHandler")), nil)
-//                }
-//        }
-        
-        self.progressBarDownload.isHidden = true
+        self.loading(currentlyLoading: false)
+        self.progressLabel.text = "0%"
         
         
         self.scrollView.minimumZoomScale = 1.0
@@ -67,10 +53,9 @@ class SWWallDetailViewController: UIViewController, UIScrollViewDelegate {
     
     @IBAction func actionButtonAction(_ sender: UIBarButtonItem) {
         
+        let alert = UIAlertController(title: "Action", message: "Save this image to your device", preferredStyle: UIAlertControllerStyle.actionSheet)
         
-        let alert = UIAlertController(title: "Action", message: "", preferredStyle: UIAlertControllerStyle.actionSheet)
-        
-        let saveRawImageAction = UIAlertAction(title: "Save Original sized Image (could take long time)", style: .default) { (action) in
+        let saveRawImageAction = UIAlertAction(title: "Save Raw Image", style: .default) { (action) in
             guard let urlToDownload = self.wall?.imageWall?.rawUrl else {
                 return
             }
@@ -78,9 +63,9 @@ class SWWallDetailViewController: UIViewController, UIScrollViewDelegate {
             self.downloadAndSaveImage(url: urlToDownload)
         }
         
-        let saveAction = UIAlertAction(title: "Save HD sized image", style: UIAlertActionStyle.default) { (action) in
+        let saveAction = UIAlertAction(title: "Save HD image", style: UIAlertActionStyle.default) { (action) in
             
-            guard let urlToDownload = self.wall?.imageWall?.smallSizeUrl else {
+            guard let urlToDownload = self.wall?.imageWall?.fullSizeUrl else {
                 return
             }
             
@@ -102,16 +87,13 @@ class SWWallDetailViewController: UIViewController, UIScrollViewDelegate {
     
     func downloadAndSaveImage(url:URL){
         print(url.absoluteString)
-        UIView.animate(withDuration: 0.2, delay: 0.0, options: [UIViewAnimationOptions.curveEaseIn], animations: {
-                self.progressBarDownload.isHidden = false
-            }) { done in
-                self.progressBarDownload.isHidden = false
-            }
-        
+
+        self.loading(currentlyLoading: true)
         
         Alamofire.request(url)
         .downloadProgress(closure: { progress in
-            print(Float(progress.fractionCompleted.multiplied(by: 100).rounded()))
+            print(Int(progress.fractionCompleted.multiplied(by: 100).rounded(FloatingPointRoundingRule.up)))
+            self.progressLabel.text = "\(Int(progress.fractionCompleted.multiplied(by: 100).rounded(FloatingPointRoundingRule.up)))%"
             self.progressBarDownload.progress = Float(progress.fractionCompleted)
         })
         .responseData { response in
@@ -123,36 +105,35 @@ class SWWallDetailViewController: UIViewController, UIScrollViewDelegate {
             UIImageWriteToSavedPhotosAlbum(image!, self , #selector(self.imageHandler), nil)
             
         }
-        
-//        Alamofire.download(url)
-//            .downloadProgress(closure: { (progress) in
-//                print("Download Progress: \(progress.fractionCompleted*100.rounded())")
-//            })
-//            .responseData { response in
-//            print(response.result.value)
-//            if let data = response.result.value {
-//                let image = UIImage(data: data)
-//                UIImageWriteToSavedPhotosAlbum(image!, self, Selector(("imageSavingCompletionHandler")), nil)
-//            }
-//        }
-//        Nuke.loadImage(with: url, into: self.wallImage!, handler: { (response, something) in
-//            print(response)
-//            print(something)
-//            UIImageWriteToSavedPhotosAlbum(self.wallImage.image!, self , #selector(self.imageHandler), nil)
-//        })
     }
     
     func imageHandler(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafeRawPointer) {
-            UIView.animate(withDuration: 0.2, delay: 0.0, options: [UIViewAnimationOptions.curveEaseIn], animations: {
-                self.progressBarDownload.isHidden = true
-            }) { done in
-                self.progressBarDownload.isHidden = true
-            }
+        self.loading(currentlyLoading: false)
         guard error == nil else {
             //Error saving image
             return
         }
 
+    }
+    
+    func loading(currentlyLoading status:Bool){
+        if status {
+            UIView.animate(withDuration: 0.3, delay: 0.0, options: [UIViewAnimationOptions.transitionCurlUp], animations: {
+                self.progressBarDownload.isHidden = false
+                self.loaderView.isHidden = false
+            }) { done in
+                self.progressBarDownload.isHidden = false
+                self.loaderView.isHidden = false
+            }
+        }else{
+            UIView.animate(withDuration: 0.25, delay: 0.0, options: [UIViewAnimationOptions.curveEaseInOut], animations: {
+                self.progressBarDownload.isHidden = true
+                self.loaderView.isHidden = true
+            }) { done in
+                self.progressBarDownload.isHidden = true
+                self.loaderView.isHidden = true
+            }
+        }
     }
     
     @IBAction func infosButtonAction(_ sender: UIBarButtonItem) {
